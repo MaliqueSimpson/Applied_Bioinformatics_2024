95b5ca2576486f58bbb8cc9748efd773e5a175ae
MaliqueSimpson <malsimpson@claflin.edu>
2024-09-18 14:23:53 -0400

    Add files via upload

---
+++ Phylogenetic-Analysis.R
@@ -0,0 +1,4 @@
+#Author: Malique Simpson; Date: September 18, 2024; Purpose: create dummy data, generate phylogentic tree plot
+# Load tree libraries
+#Generate some dummy data 
+text.string<- "(((((((cow, pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fish),shark)"

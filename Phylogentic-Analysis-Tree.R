f27bc5e7318c6aa27c474ec55df7a428d1937797
MaliqueSimpson <malsimpson@claflin.edu>
2024-09-25 09:10:08 -0400

    Add files via upload

---
+++ Phylogentic-Analysis-Tree.R
@@ -0,0 +1,70 @@
+%PDF-1.4
+%âãÏÓ\r
+1 0 obj
+<<
+/CreationDate (D:20240925090945)
+/ModDate (D:20240925090945)
+/Title (R Graphics Output)
+/Producer (R 4.4.1)
+/Creator (R)
+>>
+endobj
+2 0 obj
+<< /Type /Catalog /Pages 3 0 R >>
+endobj
+5 0 obj
+<< /Type /Page /Parent 3 0 R /Contents 6 0 R /Resources 4 0 R >>
+endobj
+6 0 obj
+<<
+/Length 559 /Filter /FlateDecode
+>>
+stream
+xœ•UÉÚ@½û+ê‡Tºz÷u¤I$¤HY|Csğ0–HÊß§İ.CwâHñÁ¯ôº–×2Ávğ³ø‡ª(„€ô}Zï?¨Àª^4r¼õXKJÖµƒjËÙçÍ¤€Ù¯ö¸Ã;
+?›CsŞ® šƒèÔ4ó'¨ÅcKÿ]÷ëÇ‚Ğ¸Ë'ğR,Â³+(–şT(ƒÆ‚R„BÁH„.ä€[€o‡¤Ó¨¨£Xƒ¢pOáH2P¤,ÑÛG
+‡ˆJ´E™µp¤pÈz60ŒQèˆa$pD9¤®WKµg	!Û^¡4S‚qèÌĞÃ´DYvêÜ=L›$GHå½KÆi
+7)“*ŒSÊM0î+l˜ÅS>LSŠ”2‡q*»ôm¢ã,‹ÕÙÌŒSŠ&›ˆqj¥sí§FR>WŸ1«Û¹QË\~Æ	ãæ×ìÀ¸]³ª£vÍ{µk®ã˜]scŒù5óÖ¨_SwZ)÷Í¸O2SŒßpv‘òï¥¥yiÉ?–VÄƒ)*Ûï¬Õqd`v½-¦úÿ².f™ıØ†}Wí&.ÃiO_7uÛL>OÊv–ê<×çéÇCé{ÚfDûùrš®‚—cú>6—}}˜Ü‰Fd%NÇçíô*¸Kö±]_êC==ƒ–,FğCÓÖ«úpŞLÏşlNÄ,ëcû¯Û·éI´|±œ½mêS÷öü~ÿn¿lS”ëendstream
+endobj
+3 0 obj
+<< /Type /Pages /Kids [ 5 0 R ] /Count 1 /MediaBox [0 0 959 445] >>
+endobj
+4 0 obj
+<<
+/ProcSet [/PDF /Text]
+/Font <</F3 8 0 R /F4 9 0 R >>
+/ExtGState << >>
+>>
+endobj
+7 0 obj
+<<
+/Type /Encoding /BaseEncoding /WinAnsiEncoding
+/Differences [ 45/minus 96/quoteleft
+144/dotlessi /grave /acute /circumflex /tilde /macron /breve /dotaccent
+/dieresis /.notdef /ring /cedilla /.notdef /hungarumlaut /ogonek /caron /space]
+>>
+endobj
+8 0 obj
+<< /Type /Font /Subtype /Type1 /Name /F3 /BaseFont /Helvetica-Bold
+/Encoding 7 0 R >>
+endobj
+9 0 obj
+<< /Type /Font /Subtype /Type1 /Name /F4 /BaseFont /Helvetica-Oblique
+/Encoding 7 0 R >>
+endobj
+xref
+0 10
+0000000000 65535 f 
+0000000021 00000 n 
+0000000163 00000 n 
+0000000922 00000 n 
+0000001005 00000 n 
+0000000212 00000 n 
+0000000292 00000 n 
+0000001096 00000 n 
+0000001353 00000 n 
+0000001454 00000 n 
+trailer
+<< /Size 10 /Info 1 0 R /Root 2 0 R >>
+startxref
+1558
+%%EOF
